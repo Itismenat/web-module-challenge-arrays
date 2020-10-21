df4be58b439e025674c03e4ab952138d86da51b6
Itismenat <65092546+Itismenat@users.noreply.github.com>
2020-10-20 22:25:23 -0500

    task 3 done

--- index.js
+++ index.js
@@ -84,6 +84,9 @@ function addFlavor(originalFlavors) {
   console.log(originalFlavors);
 }
 addFlavor(originalFlavors);
+originalFlavors.unshift("Rainbow Sherbert");
+addFlavor(originalFlavors);
+
 /* 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 4: 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀
 Houston, we have a problem! There are now 32 flavors in the originalFlavors array! Your task is to remove an item from the end of the array. 
 
